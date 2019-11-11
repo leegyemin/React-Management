76adaaf9ba289d4d20e59821c183a9dcc8701e5b
gyemin.id <ppmt2018!>
2019-11-11 16:46:44 +0900

    - Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import {withStyles} from '@material-ui/core/styles'
 const styles = theme => ({
     root: {
         width: '100%',
-        marginTop: theme.spacing.unit * 3,
+        marginTop: theme.spacing(3),
         overflowX: 'auto'
     },
     table: {
@@ -20,34 +20,23 @@ const styles = theme => ({
     }
 });
 
-const customers = [
-    {
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/1',
-        'name': '홍길동',
-        'birthday': '870128',
-        'gender': '남',
-        'job': '대학생'
-    },
-    {
-        'id': 2,
-        'image': 'https://placeimg.com/64/64/2',
-        'name': '이계민',
-        'birthday': '111111',
-        'gender': '남',
-        'job': '프로그래머'
-    },
-    {
-        'id': 3,
-        'image': 'https://placeimg.com/64/64/3',
-        'name': '김성남',
-        'birthday': '888888',
-        'gender': '여',
-        'job': '디자이너'
-    }
-];
-
 class App extends Component {
+    state = {
+        customers: ""
+    };
+
+    componentDidMount() {
+        this.callApi().then(res => {
+            this.setState({customers: res})
+        }).catch(err => console.log(err))
+    }
+
+    callApi = async () => {
+        const response = await fetch('/api/customers');
+        const body = await response.json();
+        return body;
+    };
+
     render() {
         const {classes} = this.props;
         return (
@@ -64,8 +53,8 @@ class App extends Component {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {
-                            customers.map(c => {
+                        {this.state.customers ?
+                            this.state.customers.map(c => {
                                 return <Customer
                                     key={c.id}
                                     id={c.id}
@@ -74,7 +63,7 @@ class App extends Component {
                                     birthday={c.birthday}
                                     gender={c.gender}
                                     job={c.job}/>
-                            })
+                            }) : ""
                         }
                     </TableBody>
                 </Table>
@@ -83,4 +72,4 @@ class App extends Component {
     }
 }
 
-export default withStyles(styles)(App) ;
+export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -6,6 +6,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '홍길동',
+            'birthday': '870128',
+            'gender': '남',
+            'job': '대학생'
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '이계민',
+            'birthday': '111111',
+            'gender': '남',
+            'job': '프로그래머'
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '김성남',
+            'birthday': '888888',
+            'gender': '여',
+            'job': '디자이너'
+        }
+    ])
+});
+
 app.get('/api/hello', (req, res) => {
     res.send({message: "Hello ~ "});
 });
